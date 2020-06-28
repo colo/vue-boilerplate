a7a93c8f1d20ad91f92c035f07c1a92ee6c6803b
Luciano Bueno <glamero@gmail.com>
2020-06-27 22:13:44 -0300

    bump

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+// make sure to set this synchronously immediately after loading Vue and before `new Vue`
+Vue.config.devtools = true
+Vue.config.performance = process.env.NODE_ENV !== 'production'
 
 new Vue({
   router,
